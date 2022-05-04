d2ebfe27c7d004d9d926f26cb162e0ab79a50c47
CHHAY davy <davychhay14@gmail.com>
2022-05-05 01:39:43 +0200

    Mise a jour de gamelist en implementant la liste de joueurs

--- includes/gameList.c
+++ includes/gameList.c
@@ -3,28 +3,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include "./playerList.c"
 
 typedef struct gamelist{
 
-	struct gamelist *next;
+	struct gamelist *next; // Parti suivante
 	uint8_t num;  // Numero de partie
-  	uint8_t nb_player; // Nombre de joueurs
-  	// TODO: Ajouter une liste de joueurs
+	uint8_t nb_player; // Nombre de joueurs
+  	Playerlist playerlist;
 }Gamelist;
 
 int ind = 1;
 
-void add(Gamelist **list){
+
+
+uint8_t add(Gamelist **list){
 	Gamelist *element = malloc(sizeof(Gamelist));
 	assert(element != NULL);
 	element -> num = ind;
 	ind = ind + 1;
-	element -> nb_player = 1;
 	element ->next = *list;
 	*list = element;
+	return element -> num;
 	
 	
 }
+
+int addPlayerTo(Gamelist *list, uint8_t ngame , char *i , char*p){
+	while(list != NULL){
+		if(ngame == list -> num){
+			Playerlist *pl = &(list ->playerlist);
+			addPlayer(&pl,i,p);
+			list -> nb_player = (list -> nb_player) + 1;
+			return 1;
+		}
+		list = list -> next;
+	}
+	return 0;
+}
+
+Gamelist* search(Gamelist * list,uint8_t n){
+	while(list != NULL){
+		if(n == list -> num){
+			return list;
+		}
+		list = list -> next;
+	}
+	return NULL;
+	
+}
+
 int length(Gamelist *list){
 	int i = 0;
 	while(list != NULL){
@@ -54,16 +82,22 @@ int rmv(Gamelist *list,uint8_t n){
 	return 0;
 }
 
-/* void main(){
+ /*void main(){
 	Gamelist *test = NULL;
+	printf("%d \n",length(test));
 	add(&test);
 	add(&test);
 	add(&test);
+	uint8_t n = 12;
+	Gamelist *res = search(test,n);
+	n = 0;
+	n = res -> num;
+	printf("%u \n",res -> num);
 	printf("%d \n",length(test));
 	uint8_t v = 2;
 	printf("RMV : %d\n",rmv(test,v));
 	printf("%d \n",length(test));
 	
-} */
+}  */
 
 
