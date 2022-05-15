becfe3bb7831d5abfa2988e9c11ee9379cceb6cc
Ibrahima <ibrahima.bamoctar@gmail.com>
2022-05-15 23:55:57 +0200

    Avancement labyrinthe

--- Labyrinthe/Configuration.java
+++ Labyrinthe/Configuration.java
@@ -56,13 +56,93 @@ public class Configuration {
         System.out.println();
     }
 
-    /*public void nouvelleConfiguration(){
-        for(int i=0; i<this.hauteur; i++){
-            for(int j=0; j<this.largeur; j++){
-                initialisationLabyrinthe();
+    public void bonneConfiguration(){
+            //System.out.println("HAUT");
+            
+
+            for(int i=0; i<this.hauteur; i++){
+                for(int j=0; j<this.largeur; j++){
+                    if (i-1>0 && i+1<hauteur && j-1>0 && j+1<largeur) { 
+                        if(tab[i-1][j]==0 && tab[i+1][j]==0 && tab[i][j-1]==0 && tab[i][j+1]==0){
+                            if (tab[i][j]==1) { 
+                                tab[i][j]=0; 
+                            }
+                        }
+                    }
+                }
             }
+
+            /*System.out.println("BAS");
+            if (a + 1 < hauteur) { 
+                if (tab[a + 1][b] == 0) {
+                    tab[a + 1][b] = 1;
+                }
+            initialisationLabyrinthe(a + 1, b,compteur+1);
+            }
+
+            System.out.println("GAUCHE");
+            if (b - 1 >= 0) {
+                if (tab[a][b - 1] == 0) { 
+                    tab[a][b - 1] = 1;
+                }
+                initialisationLabyrinthe(a, b - 1, compteur+1);
+            }
+
+            System.out.println("DROITE");
+            if (b + 1 < largeur) {
+                if (tab[a][b + 1] == 0) {
+                    tab[a][b + 1] = 1;
+                }
+                initialisationLabyrinthe(a, b + 1, compteur+1);
+            }*/
+        
+    }
+
+    /*public void initialisationLabyrinthe(int a, int b, int compteur){
+        this.tab[a][b]=1;
+
+        Random rand = new Random();
+        int dir = rand.nextInt(4);
+        System.out.println(dir);
+
+        if(compteur<=100){
+            if(dir==0){
+                System.out.println("HAUT");
+                if (a-1 >= 0) { 
+                    if (tab[a - 1][b]==0) { 
+                        tab[a - 1][b]=1; 
+                    }
+                    initialisationLabyrinthe(a-1,b,compteur+1);
+                }
+            }else if(dir==1){
+                System.out.println("BAS");
+                if (a + 1 < hauteur) { 
+                    if (tab[a + 1][b] == 0) {
+                        tab[a + 1][b] = 1;
+                    }
+                initialisationLabyrinthe(a + 1, b,compteur+1);
+                }
+            }else if(dir==2){
+                System.out.println("GAUCHE");
+                if (b - 1 >= 0) {
+                    if (tab[a][b - 1] == 0) { 
+                        tab[a][b - 1] = 1;
+                    }
+                    initialisationLabyrinthe(a, b - 1, compteur+1);
+                }
+            }else if(dir==3){
+                System.out.println("DROITE");
+                if (b + 1 < largeur) {
+                    if (tab[a][b + 1] == 0) {
+                        tab[a][b + 1] = 1;
+                    }
+                    initialisationLabyrinthe(a, b + 1, compteur+1);
+                }
+            }
+            System.out.println(compteur);
         }
     }*/
+
 
     public void afficheLabyrinthe(){
 		for(int i=0; i<this.largeur; i++){
@@ -83,8 +163,9 @@ public class Configuration {
 
     public static void main(String[] args) {
         Jeu j = new Jeu();
-        Configuration conf = new Configuration(4,4);
+        Configuration conf = new Configuration(7,7);
         conf.initialisationLabyrinthe();
+        conf.bonneConfiguration();
         j.newGame();
         System.out.println("Voici le labyrinthe : ");
         System.out.println("Légende : 0 = mur et 1 = case libre");
--- Labyrinthe/Jeu.java
+++ Labyrinthe/Jeu.java
@@ -3,20 +3,12 @@ import java.util.Scanner;
 
 
 public class Jeu {
-    private Scanner sc;
-
-    //Construction du constructeur
-    public Jeu(){
-        this.sc = new Scanner(System.in);
-    }
 
     public void newGame(){
        System.out.println("------------");
        System.out.println("| GhostLab |");
        System.out.println("------------");
-       System.out.println("Entrez votre prenom :");
-       String name = sc.nextLine();
-       System.out.println("Bienvenue sur GhostLab, "+name+" !");
+       System.out.println("Bienvenue sur GhostLab !");
     }
 
     //méthodes pour les règles du jeu
