4d512049f80fc4830ac52d1ecb5318d76b951f24
Ibrahima <ibrahima.bamoctar@gmail.com>
2022-05-15 08:58:07 +0200

    Implementation debut Lab

---
+++ Labyrinthe/Jeu.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.Scanner;
+
+
+public class Jeu {
+    private Scanner sc;
+
+    //Construction du constructeur
+    public Jeu(){
+        this.sc = new Scanner(System.in);
+    }
+
+    public void newGame(){
+       System.out.println("------------");
+       System.out.println("| GhostLab |");
+       System.out.println("------------");
+       System.out.println("Entrez votre prenom :");
+       String name = sc.nextLine();
+       System.out.println("Bienvenue sur GhostLab, "+name+" !");
+    }
+
+    public static void main(String[] args) {
+        Jeu j = new Jeu();
+        j.newGame();
+    }
+}
