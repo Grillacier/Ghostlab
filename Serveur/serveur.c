f8521c3158ad59743053a192231f7790784a1682
CHHAY davy <davychhay14@gmail.com>
2022-05-17 18:19:31 +0200

    Amélioration de la lisibilité, correction erreur de NEWPL et debut de REGIS

--- Serveur/serveur.c
+++ Serveur/serveur.c
@@ -21,23 +21,10 @@ Gamelist *list = NULL;
 //Verrou
 pthread_mutex_t verrou = PTHREAD_MUTEX_INITIALIZER;
 
-void *lobby(void* sock){
-         int sock2 = *(int*)sock;
-	 pthread_mutex_lock(&verrou);
-	 if(sendGames(sock2,nbgame) == 0){
-	 	printf("ERREUR");
-	 }
-	 Gamelist *threadList = list;
-	 
-	 while(threadList != NULL){
-	 	sendOgame(sock2, threadList ->num , threadList ->nb_player);
-	 	threadList = threadList -> next;
-	}
-
-     	 pthread_mutex_unlock(&verrou);
-     	 
-     	 while(1){
+int chooseGame(int sock){
+	 while(1){
      	 void *receiver = malloc(24);
+	 Gamelist *threadList = list;
      	 if(recv(sock2,receiver,sizeof(char)*20 + sizeof(uint8_t),0) == 0){
      	 	perror("Cannot receive registration");
      	 	
@@ -52,32 +39,78 @@ void *lobby(void* sock){
      	 	memmove(&id,receiver+sizeof(char)*6,sizeof(char)*8);
      	 	id[8] = '\0';
      	 	char p[5];
-     	 	memmove(&p,receiver+sizeof(char)*6,sizeof(char)*8);
+     	 	memmove(&p,receiver+sizeof(char)*15,sizeof(char)*4);
      	 	p[4] = '\0';
      	 	
-     	 	threadList = list;
+     	 	GameList threadList = list;
      	 	printf("Creation of new game...");
      	 	pthread_mutex_lock(&verrou);
      	 	uint8_t nwgame = add(&list);
-     	 	threadList = list;
      	 	addPlayerTo(threadList,nwgame,id,p);
      	 	
      	 	pthread_mutex_unlock(&verrou);
      	 	if(nwgame != 0){
-     	 		sendRegok(sock2,nwgame);
+     	 		sendRegok(sock,nwgame);
+			return 1;
+
      	 	}
+		sendRegno(sock);
+		return 0;
      	 	
      	 	
      	 	
      	 }
      	 else if(strcmp(request,"REGIS")){
-     	 	printf("TO DO");
+     	 	char id[9];
+     	 	memmove(&id,receiver+sizeof(char)*6,sizeof(char)*8);
+     	 	id[8] = '\0';
+     	 	char p[5];
+     	 	memmove(&p,receiver+sizeof(char)*16,sizeof(char)*4);
+     	 	p[4] = '\0';
+		uint8_t id_game = 0;
+		memmove(&id_game,receiver + sizeof(char)*21,sizeof(uint8_t));
+		pthread_mutex_lock(&verrou);
+		if(addPlayerTo(threadList,id_game,id,p) == 0){
+			sendRegno(sock);
+			return 0;
+		}
+		pthread_mutex_unlock(&verrou);
+		sendRegok(sock,id_game);
+		return id_game;
      	 }
      	 
      	 }
-     	while(1){
-     		printf("DONE"); 
-     	}
+
+}
+
+void *lobby(void* sock){
+	// Variable de thread
+         int sock2 = *(int*)sock;
+	 uint8_t ur_game_id = 0;
+	 char ur_id [8] = {NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL};
+	// ------
+	 pthread_mutex_lock(&verrou);
+	 if(sendGames(sock2,nbgame) == 0){
+	 	printf("ERREUR");
+	 }
+	 Gamelist *threadList = list;
+	 
+	 while(threadList != NULL){
+	 	sendOgame(sock2, threadList ->num , threadList ->nb_player);
+	 	threadList = threadList -> next;
+	}
+
+     	 pthread_mutex_unlock(&verrou);
+     	 
+     	 while(1){
+		ur_game_id = chooseGame(sock2);
+		while(ur_game_id != 0){
+			printf(" Vous etes inscrit, TODO");
+		}
+		printf("Vous n'etes pas inscrit");
+	}
+     	
+     	
     } 
      
      
